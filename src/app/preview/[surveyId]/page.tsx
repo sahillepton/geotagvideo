ed754e7161cbcd93a68c7deb8f5fab78392d2f63
sahillepton <sahil@leptonsoftware.com>
2025-09-15 10:43:06 +0530

    fixes

--- src/app/preview/[surveyId]/page.tsx
+++ src/app/preview/[surveyId]/page.tsx
@@ -43,13 +43,65 @@ const PreviewPage = async ({
   const { data: videoData, error: videoError } = videoResult;
   const { data: surveyData, error: surveyError } = surveyResult;
 
+  if (!videoData?.url) {
+    // console.log("no video found for this survey");
+    return (
+      <div className="flex flex-col justify-center items-center h-screen gap-4">
+        <Badge
+          variant="destructive"
+          className="text-2xl font-bold bg-red-500 text-white animate-pulse border-red-700 shadow-lg"
+        >
+          No video found for this survey
+        </Badge>
+        <Link
+          href="/geotaggedvideos"
+          className="text-red-500 hover:text-red-700 underline text-sm"
+        >
+          Go back to surveys?
+        </Link>
+      </div>
+    );
+  }
+
+  if (!videoData.mux_playback_id) {
+    //  console.log("video is still processing");
+    return (
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1 className="text-2xl font-bold">Video is still processing</h1>
+        <Link
+          href="/geotaggedvideos"
+          className="text-gray-800 hover:text-gray-600 underline text-sm"
+        >
+          Go back to surveys?
+        </Link>
+      </div>
+    );
+  }
+
   return (
-    <Suspense
-      fallback={
-        <div className="flex flex-col justify-center items-center h-screen">
-          <Badge
-            variant={"secondary"}
-            className="
+    <div className="px-4 ">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4">
+          <Button variant="outline">
+            <Link
+              href="/geotaggedvideos"
+              className="flex items-center gap-2 w-fit h-fit"
+            >
+              <ArrowLeftIcon size={16} />
+              Back
+            </Link>
+          </Button>
+        </div>
+        <h1 className="text-2xl font-extrabold tracking-tight text-balance text-[#262626] dark:text-white">
+          {videoData.name}
+        </h1>
+      </div>
+      <Suspense
+        fallback={
+          <div className="flex flex-col justify-center items-center h-screen">
+            <Badge
+              variant={"secondary"}
+              className="
     text-2xl 
     font-bold 
     bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-200 
@@ -58,34 +110,22 @@ const PreviewPage = async ({
     text-yellow-800
     shadow-lg
     "
-          >
-            Fetching video and GPS tracks
-          </Badge>
-        </div>
-      }
-    >
-      <div className="w-[1200px] flex justify-between items-center ml-4 mr-4 mt-4">
-        <p className="text-2xl font-bold">{videoData.name}</p>
-        <Feedback videoId={videoData.id} />
-      </div>
-      {videoData.mux_playback_id ? (
-        <div className="p-4">
+            >
+              Fetching video and GPS tracks
+            </Badge>
+          </div>
+        }
+      >
+        <div className=" mt-4">
           <VideoWithMap
-            videoUrl={videoData?.mux_playback_id}
+            videoUrl={`https://stream.mux.com/${videoData.mux_playback_id}.m3u8`}
             locationData={surveyData?.gps_tracks?.location_data}
             initialX={x}
             initialY={y}
           />
         </div>
-      ) : (
-        <div>
-          <MP4VideoWithMap
-            videoUrl={videoData?.url}
-            locationData={surveyData?.gps_tracks?.location_data}
-          />
-        </div>
-      )}
-    </Suspense>
+      </Suspense>
+    </div>
   );
 };
 
