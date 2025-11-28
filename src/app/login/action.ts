be9670b8d95cdcea7aff39ab892fbdae9b14c8da
sahillepton <sahil@leptonsoftware.com>
2025-11-28 08:46:28 +0530

    remove password validation

--- src/app/login/action.ts
+++ src/app/login/action.ts
@@ -4,57 +4,53 @@ import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
-export type FormState = {errors ? : { email ? : string[], password ?: string[]}} | undefined
-export async function login(state: FormState, formData: FormData, ) {
-    const schema = z.object({
-        email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
-  password: z
-    .string()
-    .min(8, { message: 'Be at least 8 characters long' })
-    .trim(),
-    });
+export type FormState =
+  | { errors?: { email?: string[]; password?: string[] } }
+  | undefined;
+export async function login(state: FormState, formData: FormData) {
+  const schema = z.object({
+    email: z.string().email({ message: "Please enter a valid email." }).trim(),
+    password: z.string().trim(),
+  });
 
-    const validatedFields = schema.safeParse({
-        email: formData.get('email'),
-        password: formData.get('password'),
-    });
+  const validatedFields = schema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 
-    if (!validatedFields.success) {
-        return {
-          errors: validatedFields.error.flatten().fieldErrors,
-        }
-    }
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
 
-    const { email, password } = validatedFields.data;
+  const { email, password } = validatedFields.data;
 
-    try {
-
-
-
+  try {
     const supabase = await createClient();
 
     const { data: userData, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("email", email)
-    .single();
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .single();
 
     if (!userData || userData.password !== password) {
-        return {
-            errors: {
-                password: ['Invalid email or password'],
-            },
-        }
+      return {
+        errors: {
+          password: ["Invalid email or password"],
+        },
+      };
     }
 
-        (await cookies()).set("user", JSON.stringify(userData), {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            maxAge: 60 * 60 * 24 * 30,
-            path: "/",
-        })
-        revalidatePath("/login")
-    } catch (error) {
-        console.error(error);
-    }
+    (await cookies()).set("user", JSON.stringify(userData), {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 30,
+      path: "/",
+    });
+    revalidatePath("/login");
+  } catch (error) {
+    console.error(error);
+  }
 }
